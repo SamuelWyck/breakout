64a1204d00334fc98789e9eb9df04e99bae5550b
Samuel <samawyck@gmail.com>
2026-05-04 16:51:56 -0500

    Pass presentation flag to display class

--- framework/framework.h
+++ framework/framework.h
@@ -2,11 +2,12 @@
 #define FRAMEWORK_H
 
 
+#include <SDL3/SDL_render.h>
 #include "./display.h"
 
 
 namespace Framework {
-    inline static Display display{1536, 864};
+    inline static Display display{1536, 864, SDL_LOGICAL_PRESENTATION_STRETCH};
 };
 
 
