f0d01c9919c089cb845963ea9da7e5e9a7591283
Samuel <samawyck@gmail.com>
2026-05-04 12:13:23 -0500

    Factor out display logic

---
+++ game.h
@@ -0,0 +1,66 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+#include <SDL3/SDL.h>
+#include <SDL3_image/SDL_image.h>
+#include "./framework/framework.h"
+
+
+class Game {
+
+
+public:
+    Game() {
+    };
+
+    ~Game() {
+    };
+
+    Game(const Game&) = delete;
+    Game(Game&&) = delete;
+    Game& operator=(Game&&) = delete;
+    Game& operator=(const Game&) = delete;
+
+
+    void startGame() {
+        gameLoop();
+    };
+
+
+private:
+    void gameLoop() {
+        bool running {true};
+        SDL_Texture* img {IMG_LoadTexture(Framework::display.renderer(), "C:\\Users\\samaw\\.vscode\\breakout\\wall2.png")};
+        SDL_SetTextureScaleMode(img, SDL_SCALEMODE_PIXELART);
+        if (!img) {
+            SDL_Log(SDL_GetError());
+        }
+
+        SDL_FRect srcRect{Framework::display.canvasWidth() / 2.0f - 20, Framework::display.canvasHeight() / 2.0f - 20, 40, 40};
+
+
+        while (running) {
+            SDL_Event event;
+            while (SDL_PollEvent(&event)) {
+                if (event.type == SDL_EVENT_QUIT) {
+                    running = false;
+                } else if (event.type == SDL_EVENT_KEY_DOWN) {
+                    if (event.key.scancode == SDL_SCANCODE_BACKSPACE) {
+                        running = false;
+                    }
+                }
+            }
+
+
+            SDL_SetRenderDrawColor(Framework::display.renderer(), 255, 0, 0, 255);
+            SDL_RenderClear(Framework::display.renderer());
+            SDL_RenderTexture(Framework::display.renderer(), img, nullptr, &srcRect);
+            SDL_RenderPresent(Framework::display.renderer());
+        }
+    };
+};
+
+
+
+#endif
