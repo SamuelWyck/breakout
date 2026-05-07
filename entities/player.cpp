5d1db5f5c82d36a43299124c74eaeb64ebad176c
Samuel <samawyck@gmail.com>
2026-05-07 09:58:39 -0500

    Add methods and controls to handle loading and launching the ball

--- entities/player.cpp
+++ entities/player.cpp
@@ -1,6 +1,7 @@
 #include <SDL3/SDL_render.h>
 #include "../utils/fRect.h"
 #include "./player.h"
+#include "./ball.h"
 #include "../controller/playerController.h"
 
 
@@ -15,29 +16,73 @@ Player::Player(float centerX, float centerY, int speed, const PlayerController& 
 
 
 void Player::update(SDL_Renderer* renderer, double deltaTime, const FRect& screenRect) {
-    handleMovement(screenRect, deltaTime);
+    handleInputs(screenRect, deltaTime);
+
+    if (m_launchBall) {
+        m_launchBall = false;
+        launchBall();
+    }
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderFillRect(renderer, &m_rect.getSDLFRect());
 };
 
 
-void Player::handleMovement(const FRect& screenRect, double deltaTime) {
+void Player::loadBall(Ball* ballPtr) {
+    m_ballPtr = ballPtr;
+    m_ballPtr->stop();
+    positionBall();
+};
+
+
+void Player::positionBall() {
+    float ballCenterX {m_rect.centerX()};
+    float ballCenterY {m_rect.centerY() - (m_rect.height() / 2) - m_ballPtr->m_hitbox.radius()};
+    m_ballPtr->setCenter(ballCenterX, ballCenterY);
+};
+
+
+void Player::launchBall() {
+    float ballXSpeedDelta {0.0f};
+    if (m_movingLeft && !m_movingRight) {
+        ballXSpeedDelta = -1 * m_ballXSpeedDelta;
+    } else if (m_movingRight && !m_movingLeft) {
+        ballXSpeedDelta = m_ballXSpeedDelta;
+    }
+    m_ballPtr->launch(ballXSpeedDelta);
+    m_ballPtr = nullptr;
+};
+
+
+void Player::handleInputs(const FRect& screenRect, double deltaTime) {
     auto pressedInputs {m_controller.getPressedInputs()};
+    m_movingLeft = false;
+    m_movingRight = false;
 
 
     if (pressedInputs["LEFT"]) {
         m_rect.setX(m_rect.x() - (m_speed * deltaTime));
+        m_movingLeft = true;
     }
     if (pressedInputs["RIGHT"]) {
         m_rect.setX(m_rect.x() + (m_speed * deltaTime));
+        m_movingRight = true;
+    }
+    if (pressedInputs["LAUNCH"] && m_ballPtr) {
+        m_launchBall = true;
     }
 
     if (!screenRect.containsRect(&m_rect)) {
         if (m_rect.x() < 0.0f) {
             m_rect.setX(0.0f);
+            m_movingLeft = false;
         } else if ((m_rect.x() + m_rect.width()) > screenRect.right()) {
             m_rect.setRight(screenRect.right());
+            m_movingRight = false;
         }
+    }
+
+    if (m_ballPtr != nullptr) {
+        positionBall();
     }
 };
--- entities/player.h
+++ entities/player.h
@@ -5,13 +5,21 @@
 #include <SDL3/SDL_render.h>
 #include "../utils/fRect.h"
 #include "../controller/playerController.h"
+#include "./ball.h"
 
 
 class Player {
     FRect m_rect{};
-    int m_speed {};
 
+    int m_speed {};
+    bool m_movingLeft {false};
+    bool m_movingRight {false};
+    
     const PlayerController& m_controller{};
+
+    bool m_launchBall {false};
+    Ball* m_ballPtr {nullptr};
+    float m_ballXSpeedDelta {3.0f};
 
 
 public:
@@ -19,9 +27,15 @@ public:
 
     void update(SDL_Renderer* renderer, double deltaTime, const FRect& screenRect);
 
+    void loadBall(Ball* ballPtr);
+
 
 private:
-    void handleMovement(const FRect& screenRect, double deltaTime);
+    void handleInputs(const FRect& screenRect, double deltaTime);
+
+    void positionBall();
+
+    void launchBall();
 };
 
 
