97749705119bec049b7b3be1a841aa753a4701ec
Samuel <samawyck@gmail.com>
2026-05-08 15:15:22 -0500

    Add default constructor to allow for use in unordered_map and add setters

--- entities/basicBlock.cpp
+++ entities/basicBlock.cpp
@@ -4,16 +4,30 @@
 
 
 
-BasicBlock::BasicBlock(float top, float left, const Color& color, int health, int scoreVal) 
+BasicBlock::BasicBlock(float x, float y, const Color& color, int health, int scoreVal) 
     : m_color{color}, m_health{health}, m_scoreValue{scoreVal} 
 {
-    m_rect.setTopleft(top, left);
+    m_rect.setTopleft(x, y);
     m_rect.setSize(80, 40);
+};
+
+
+BasicBlock::BasicBlock() {
 };
 
 
 bool BasicBlock::isDead() const {
     return m_health <= 0;
+};
+
+
+void BasicBlock::setTopLeft(float x, float y) {
+    m_rect.setTopleft(x, y);
+};
+
+
+void BasicBlock::setColor(const Color& color) {
+    m_color = color;
 };
 
 
