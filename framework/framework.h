e2f33cd4105af6dae3bab5eac50dca389f5147b1
Samuel <samawyck@gmail.com>
2026-05-10 12:44:15 -0500

    Separate code

--- framework/framework.h
+++ framework/framework.h
@@ -4,18 +4,13 @@
 
 #include <SDL3/SDL_render.h>
 #include <SDL3/SDL_video.h>
+#include "../assetLoaders/images.h"
 #include "./display.h"
 
 
 namespace Framework {
-    // pass screen size, canvas size, window flags, and renderer flags 
-
-    inline static Display display{
-        0, 0, // screen size
-        1536, 864, // canvas size
-        SDL_WINDOW_FULLSCREEN | SDL_WINDOW_MOUSE_GRABBED | SDL_WINDOW_MOUSE_RELATIVE_MODE, // window flags 
-        SDL_LOGICAL_PRESENTATION_STRETCH // renderer presentation flag
-    };
+    extern Display display;
+    extern Images images;
 };
 
 
