cbd7d2ed23064570654af566a49eba8a653e2edc
Samuel <samawyck@gmail.com>
2026-05-26 13:10:49 -0500

    Move file and split into header and source

---
+++ sdlUtils/userInterface/mouse.cpp
@@ -0,0 +1,178 @@
+#include <cmath>
+#include <fstream>
+#include <string_view>
+#include <string>
+#include <SDL3/SDL_mouse.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_rect.h>
+#include "./mouse.h"
+#include "../math.h"
+#include "../collision/fRect.h"
+
+
+
+Mouse::Mouse(
+    float screenWidth, 
+    float screenHeight, 
+    float canvasWidth, 
+    float canvasHeight,
+    SDL_Texture* img, 
+    float imgXOffset,
+    float imgYOffset,
+    std::string_view saveFilePath
+)
+    : m_img{img}, 
+    m_imgXOffset{imgXOffset},
+    m_imgYOffset{imgYOffset},
+    m_screenWidth{screenWidth}, 
+    m_screenHeight{screenHeight}, 
+    m_canvasWidth{canvasWidth}, 
+    m_canvasHeight{canvasHeight},
+    m_saveFilePath{saveFilePath}
+{
+    SDL_GetGlobalMouseState(&m_xPos, &m_yPos);
+    remapCoords(&m_xPos, &m_yPos);
+    roundCoords();
+
+    if (m_img != nullptr) {
+        m_rect.setSize(static_cast<float>(m_img->w), static_cast<float>(m_img->h));
+    }
+    m_rect.setTopleft(m_xPos - m_imgXOffset, m_yPos - m_imgYOffset);
+
+    loadSensitivitySetting();
+};
+
+
+
+void Mouse::update(float relX, float relY) {
+    remapCoords(&relX, &relY);
+    m_xPos += (relX * m_sensitivity);
+    m_yPos += (relY * m_sensitivity);
+
+    if (m_clampToCanvas) {
+        clampToCanvas();
+    }
+    roundCoords();
+
+    m_rect.setTopleft(m_xPos - m_imgXOffset, m_yPos - m_imgYOffset);
+};
+
+
+void Mouse::draw(SDL_Renderer* renderer) {
+    if (m_img == nullptr || m_mouseHidden) {
+        return;
+    }
+    SDL_RenderTexture(renderer, m_img, nullptr, &m_rect.getSDLFRect());
+};
+
+
+void Mouse::clampMouseToCanvas(bool clamp) {
+    m_clampToCanvas = clamp;
+};
+
+
+bool Mouse::mouseClamped() const {
+    return m_clampToCanvas;
+};
+
+
+SDL_FPoint Mouse::getPos() const {
+    return SDL_FPoint{m_xPos, m_yPos};
+};
+
+
+const FRect& Mouse::getRect() const {
+    return m_rect;
+};
+
+
+void Mouse::setMouseHidden(bool hide) {
+    m_mouseHidden = hide;
+};
+
+
+bool Mouse::mouseHidden() const {
+    return m_mouseHidden;
+};
+
+
+float Mouse::getSensitivity() const {
+    return m_sensitivity;
+};
+
+
+void Mouse::setSensitivity(float newSensitivity) {
+    if (newSensitivity < .1f) {
+        newSensitivity = .1f;
+    }
+
+    m_sensitivity = newSensitivity;
+};
+
+
+void Mouse::saveSensitivitySetting() const {
+    if (m_saveFilePath == "") {
+        return;
+    }
+
+    try {
+        std::ofstream file{m_saveFilePath};
+        file << m_sensitivity;
+        file.close();
+
+    } catch (...) {
+        return;
+    }
+};
+
+
+void Mouse::remapCoords(float* x, float* y) {
+    *x = Math::remap(0.0f, m_screenWidth, 0.0f, m_canvasWidth, *x);
+    *y = Math::remap(0.0f, m_screenHeight, 0.0f, m_canvasHeight, *y);
+};
+
+
+void Mouse::clampToCanvas() {
+    if (m_xPos < 0.0f) {
+        m_xPos = 0.0f;
+
+    } else if (m_xPos > m_canvasWidth) {
+        m_xPos = m_canvasWidth;
+    }
+
+    if (m_yPos < 0.0f) {
+        m_yPos = 0.0f;
+
+    } else if (m_yPos > m_canvasHeight) {
+        m_yPos = m_canvasHeight;
+    }
+};
+
+
+void Mouse::roundCoords() {
+    m_xPos = roundf(m_xPos * 1000.0f) / 1000.0f;
+    m_yPos = roundf(m_yPos * 1000.0f) / 1000.0f;
+};
+
+
+void Mouse::loadSensitivitySetting() {
+    if (m_saveFilePath == "") {
+        return;
+    }
+
+    std::ifstream file{m_saveFilePath};
+    if (file.fail()) {
+        file.close();
+        return;
+    }
+
+    try {
+        std::string data{};
+        file >> data;
+        m_sensitivity = std::stof(data);
+
+    } catch (...) {
+        file.close();
+        return;
+    }
+};
---
+++ sdlUtils/userInterface/mouse.h
@@ -0,0 +1,100 @@
+#ifndef MY_SDL_MOUSE_H
+#define MY_SDL_MOUSE_H
+
+
+#include <string_view>
+#include <string>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_rect.h>
+#include "../collision/fRect.h"
+
+
+class Mouse {
+    FRect m_rect{0.0f, 0.0f, 1.0f, 1.0f};
+    SDL_Texture* m_img {nullptr};
+    float m_imgXOffset {};
+    float m_imgYOffset {};
+
+    float m_screenWidth {};
+    float m_screenHeight {};
+    float m_canvasWidth {};
+    float m_canvasHeight {};
+
+    bool m_clampToCanvas {false};
+    bool m_mouseHidden {false};
+
+    float m_xPos {};
+    float m_yPos {};
+    float m_sensitivity {1.0f};
+
+    std::string m_saveFilePath{};
+
+
+public:
+    Mouse(
+        float screenWidth, 
+        float screenHeight, 
+        float canvasWidth, 
+        float canvasHeight,
+        SDL_Texture* img=nullptr, 
+        float imgXOffset=0.0f,
+        float imgYOffset=0.0f,
+        std::string_view saveFilePath=""
+    );
+
+    Mouse(const Mouse&) = delete;
+    Mouse(Mouse&&) = delete;
+    Mouse& operator=(const Mouse&) = delete;
+    Mouse& operator=(Mouse&&) = delete;
+    ~Mouse() = default;
+
+    // Method to update the current position of the mouse.
+    // Expects the relative mouse coords from the SDL_MOUSE_MOTION_EVENT
+    void update(float relX, float relY);
+
+    // Draw the mouse img to screen if there is one.
+    void draw(SDL_Renderer* renderer);
+
+    // Set if the mouse should be constrained to the canvas. Default is not clamped.
+    void clampMouseToCanvas(bool clamp);
+
+    // Query if the mouse is currently constrained to the canvas.
+    bool mouseClamped() const;
+
+    // Get the current mouse position.
+    SDL_FPoint getPos() const;
+
+    // Get the current mouse rect. If there is no mouse image the rect will have a width and height of 1,
+    // and the rect's topleft will be the mouse position.
+    const FRect& getRect() const;
+
+    // Set if the mouse image should be drawn. Default is not hidden.
+    void setMouseHidden(bool hide);
+
+    // Query if the mouse image is currently hidden.
+    bool mouseHidden() const;
+
+    // Get the current sensitivity setting.
+    float getSensitivity() const;
+
+    // Set the mouse sensitivity. This value will be clamped to be no less than 0.1.
+    void setSensitivity(float newSensitivity);
+
+    // Save the current sensitivity setting.
+    void saveSensitivitySetting() const;
+
+
+
+private:
+    void remapCoords(float* x, float* y);
+
+    void clampToCanvas();
+
+    void roundCoords();
+
+    void loadSensitivitySetting();
+};
+
+
+
+#endif
