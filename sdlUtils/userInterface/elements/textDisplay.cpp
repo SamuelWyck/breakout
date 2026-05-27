8701fef9e4f35b6605f4ca12f962c9c94e2868f6
Samuel <samawyck@gmail.com>
2026-05-27 13:19:50 -0500

    Add menu interface and move files

---
+++ sdlUtils/userInterface/elements/textDisplay.cpp
@@ -0,0 +1,146 @@
+#include <string>
+#include <string_view>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_surface.h>
+#include "../../font.h"
+#include "../../color.h"
+#include "../../collision/fRect.h"
+#include "./textDisplay.h"
+
+
+
+TextDisplay::TextDisplay(float x, float y, std::string_view text, Font* font, const Color& color) 
+    : m_font{font}, m_color{color} 
+{
+    m_rect.setTopleft(x, y);
+    if (isValid()) {
+        updateSurface(text);
+    }
+};
+
+TextDisplay::TextDisplay() {
+};
+
+TextDisplay::~TextDisplay() {
+    SDL_DestroyTexture(m_img);
+    SDL_DestroySurface(m_surface);
+};
+
+
+bool TextDisplay::isValid() const {
+    return m_font != nullptr;
+};
+
+
+void TextDisplay::update(SDL_Renderer* renderer) {
+    if (!isValid()) {
+        return;
+    }
+
+    if (m_img == nullptr) {
+        updateTexture(renderer);
+    }
+    
+    SDL_RenderTexture(renderer, m_img, nullptr, &m_rect.getSDLFRect());
+};
+
+
+void TextDisplay::setFont(Font* font) {
+    m_font = font;
+    
+    clearStoredRenders();
+    
+    if (isValid()) {
+        updateSurface(m_text);
+    }
+};
+
+
+void TextDisplay::changeText(std::string_view text) {
+    clearStoredRenders();
+    updateSurface(text);
+};
+
+
+
+std::pair<float, float> TextDisplay::center() const {
+    return m_rect.center();
+};
+
+
+void TextDisplay::setCenter(float x, float y) {
+    m_rect.setCenter(x, y);
+    m_centerX = x;
+    m_centerY = y;
+    m_useCenter = true;
+};
+
+
+std::pair<float, float> TextDisplay::topleft() const {
+    return m_rect.topleft();
+};
+
+
+void TextDisplay::setTopleft(float x, float y) {
+    m_rect.setTopleft(x, y);
+    m_useCenter = false;
+};
+
+
+float TextDisplay::width() const {
+    if (!isValid()) {
+        return 0;
+    }
+    return m_rect.width();
+};
+
+
+float TextDisplay::height() const {
+    if (!isValid()) {
+        return 0;
+    }
+    return m_rect.width();
+};
+
+
+void TextDisplay::setX(float x) {
+    m_useCenter = false;
+    m_rect.setX(x);
+};
+
+
+void TextDisplay::setY(float y) {
+    m_useCenter = false;
+    m_rect.setY(y);
+};
+
+
+
+void TextDisplay::updateSurface(std::string_view text) {
+    m_text = text;
+    SDL_DestroySurface(m_surface);
+    m_surface = m_font->renderSurface(text, m_color.getSDLColor());
+
+    m_rect.setSize(static_cast<float>(m_surface->w), static_cast<float>(m_surface->h));
+    if (m_useCenter) {
+        m_rect.setCenter(m_centerX, m_centerY);
+    }
+};
+
+
+void TextDisplay::updateTexture(SDL_Renderer* renderer) {
+    if (m_surface == nullptr) {
+        return;
+    }
+
+    SDL_DestroyTexture(m_img);
+    m_img = SDL_CreateTextureFromSurface(renderer, m_surface);
+};
+
+
+void TextDisplay::clearStoredRenders() {
+    SDL_DestroySurface(m_surface);
+    SDL_DestroyTexture(m_img);
+    m_img = nullptr;
+    m_surface = nullptr;
+};
---
+++ sdlUtils/userInterface/elements/textDisplay.h
@@ -0,0 +1,73 @@
+#ifndef TEXT_DISPLAY_H
+#define TEXT_DISPLAY_H
+
+
+#include <string>
+#include <utility>
+#include <string_view>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_surface.h>
+#include "./iMenuElement.h"
+#include "../../font.h"
+#include "../../color.h"
+#include "../../collision/fRect.h"
+
+
+class TextDisplay : public IMenuElement {
+    SDL_Texture* m_img {nullptr};
+    SDL_Surface* m_surface {nullptr};
+
+    FRect m_rect{};
+    bool m_useCenter {false};
+    float m_centerX {};
+    float m_centerY {};
+
+    Font* m_font {nullptr};
+    Color m_color{};
+
+    std::string m_text{};
+
+
+public:
+    TextDisplay(float x, float y, std::string_view text, Font* font, const Color& color);
+    TextDisplay();
+    TextDisplay(const TextDisplay&) = delete;
+    TextDisplay(TextDisplay&&) = delete;
+    TextDisplay& operator=(const TextDisplay&) = delete;
+    TextDisplay& operator=(TextDisplay&&) = delete;
+    ~TextDisplay();
+
+
+    bool isValid() const;
+
+    void setFont(Font* font);
+
+    void changeText(std::string_view text);
+
+    void update(SDL_Renderer* renderer) override;
+
+    std::pair<float, float> center() const override;
+    void setCenter(float x, float y) override;
+
+    std::pair<float, float> topleft() const override;
+    void setTopleft(float x, float y) override;
+
+    float width() const override;
+    float height() const override;
+
+    void setX(float x) override;
+    void setY(float y) override; 
+
+
+
+private:
+    void updateSurface(std::string_view text);
+
+    void updateTexture(SDL_Renderer* renderer);
+
+    void clearStoredRenders();
+};
+
+
+
+#endif
