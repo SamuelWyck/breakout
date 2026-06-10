1d65ee17788a97bf30e5c97c84473908aff62296
Samuel <samawyck@gmail.com>
2026-06-10 17:47:17 -0500

    Add cb type alias

---
+++ sdlUtils/userInterface/menuTypes.h
@@ -0,0 +1,15 @@
+#ifndef MY_MENU_TYPES_H
+#define MY_MENU_TYPES_H
+
+
+#include <utility>
+#include <optional>
+#include <functional>
+#include <SDL3/SDL_render.h>
+
+
+using MenuReturn = std::optional<std::pair<int, int>>;
+using MenuCb = std::function<MenuReturn(SDL_Renderer*, SDL_Texture*)>;
+
+
+#endif
