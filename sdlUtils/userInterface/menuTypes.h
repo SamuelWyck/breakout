8ad8affd67d3b18f0ca1a45001ec28626ac33289
Samuel <samawyck@gmail.com>
2026-06-11 10:46:40 -0500

    Change menu callback interface

--- sdlUtils/userInterface/menuTypes.h
+++ sdlUtils/userInterface/menuTypes.h
@@ -6,10 +6,11 @@
 #include <optional>
 #include <functional>
 #include <SDL3/SDL_render.h>
+#include <SDL3/SDL_surface.h>
 
 
 using MenuReturn = std::optional<std::pair<int, int>>;
-using MenuCb = std::function<MenuReturn(SDL_Renderer*, SDL_Texture*)>;
+using MenuCb = std::function<MenuReturn(SDL_Renderer*, SDL_Surface*)>;
 
 
 #endif
