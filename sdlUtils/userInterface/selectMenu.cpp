e3b459b1ad2c15f03759b9b103c1e3015497ec17
Samuel <samawyck@gmail.com>
2026-06-25 11:04:44 -0500

    Split files

---
+++ sdlUtils/userInterface/selectMenu.cpp
@@ -0,0 +1,368 @@
+#include "./selectMenu.h"
+#include <vector>
+#include <utility>
+#include <string>
+#include <string_view>
+#include <fstream>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_rect.h>
+#include "./elements/button.h"
+#include "./menuTypes.h"
+#include "../frameCap.h"
+#include "./mouse.h"
+#include "../collision/fRect.h"
+
+
+
+SelectMenu::SelectMenu(
+    Mouse* mouse,
+    float centerX, 
+    float centerY, 
+    int numRows, 
+    int numCols, 
+    float btnGap,
+    const std::vector<Button*>& buttons,
+    Button* exitButton,
+    Button* pageUpButton,
+    Button* pageDownButton,
+    SDL_Texture* bgImage,
+    SDL_Texture* choiceLockedImg,
+    std::string_view saveFilePath
+) 
+    : m_mouse{mouse}, 
+    m_buttons{buttons}, 
+    m_bgImage{bgImage}, 
+    m_btnLockedImg{choiceLockedImg},
+    m_saveFilePath{saveFilePath}
+{
+    float colHeight {positionBtns(numRows, numCols, centerX, centerY, btnGap)};
+    positionPageAndExitBtns(exitButton, pageUpButton, pageDownButton, colHeight, btnGap, centerX, centerY);
+
+    if (bgImage != nullptr) {
+        m_bgImageRect.setSize(static_cast<float>(bgImage->w), static_cast<float>(bgImage->h));
+        m_bgImageRect.setCenter(centerX, centerY);
+    }
+    if (choiceLockedImg) {
+        m_lockedImgRect.setSize(static_cast<float>(choiceLockedImg->w), static_cast<float>(choiceLockedImg->h));
+    }
+    if (choiceLockedImg && saveFilePath.size() != 0) {
+        loadHighestUnlockedId();
+    }
+};
+
+SelectMenu::~SelectMenu() {
+    for (Button* btn : m_buttons) {
+        delete btn;
+    }
+
+    for (Button* btn : m_interfaceBtns) {
+        delete btn;
+    }
+};
+
+
+MenuReturn SelectMenu::run(SDL_Renderer* renderer, int framerate, SDL_Surface* bgCanvas) {
+    SDL_Texture* bgTexture {nullptr};
+    if (bgCanvas != nullptr) {
+        bgTexture = SDL_CreateTextureFromSurface(renderer, bgCanvas);
+    }
+
+    bool running {true};
+    FrameCap frameCap{};
+    MenuReturn returnVal{};
+
+    int btnsLength {static_cast<int>(m_buttons.size())};
+
+    while (running) {
+        bool mousePressed {false};
+        bool mouseReleased {false};
+
+        SDL_Event event{};
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                running = false;
+                returnVal = {};
+            } else if (event.type == SDL_EVENT_MOUSE_MOTION) {
+                m_mouse->update(event.motion.xrel, event.motion.yrel);
+            } else if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    mousePressed = true;
+                }
+            } else if (event.type == SDL_EVENT_MOUSE_BUTTON_UP) {
+                if (event.button.button == SDL_BUTTON_LEFT) {
+                    mouseReleased = true;
+                }
+            }
+        }
+
+
+        SDL_FPoint mousePos{m_mouse->getPos()};
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        if (bgTexture) {
+            SDL_RenderTexture(renderer, bgTexture, nullptr, nullptr);
+        }
+        if (m_bgImage) {
+            SDL_RenderTexture(renderer, m_bgImage, nullptr, &m_bgImageRect.getSDLFRect());
+        }
+
+        for (Button* btnPtr : m_interfaceBtns) {
+            btnPtr->update(renderer, mousePos, mousePressed, mouseReleased);
+            if (btnPtr->clicked()) {
+                btnPtr->unclick();
+
+                if (btnPtr->id() == m_exitId) {
+                    returnVal = {{0, -1}};
+                    running = false;
+                } else {
+                    pageButtons(btnPtr->id() == m_pageUpId);
+                }
+            }
+        }
+
+        for (int i {m_minBtnIdx}; i < m_maxBtnIdx; i += 1) {
+            if (i >= btnsLength) {
+                break;
+            }
+
+            Button* btnPtr {m_buttons[i]};
+            btnPtr->update(renderer, mousePos, mousePressed, mouseReleased);
+            drawLockedImg(renderer, btnPtr);
+
+            if (btnPtr->clicked()) {
+                btnPtr->unclick();
+
+                if (btnUnlocked(btnPtr->id())) {
+                    returnVal = {{0, btnPtr->id()}};
+                    running = false;
+                }
+            }
+        }
+
+        m_mouse->draw(renderer);
+
+        SDL_RenderPresent(renderer);
+        frameCap.capFrames(framerate);
+    }
+
+
+    SDL_DestroyTexture(bgTexture);
+    return returnVal;
+};
+
+
+int SelectMenu::runUpdate(SDL_Renderer* renderer, const SDL_FPoint& mousePos, bool mousePressed, bool mouseReleased) {
+    if (m_bgImage) {
+        SDL_RenderTexture(renderer, m_bgImage, nullptr, &m_bgImageRect.getSDLFRect());
+    }
+
+    int returnVal {-1};
+
+    for (Button* btnPtr : m_interfaceBtns) {
+        btnPtr->update(renderer, mousePos, mousePressed, mouseReleased);
+        if (btnPtr->clicked()) {
+            btnPtr->unclick();
+            if (btnPtr->id() == m_exitId) {
+                returnVal = -2;
+            } else {
+                pageButtons(btnPtr->id() == m_pageUpId);
+            }
+        }
+    }
+
+    int btnsLength {static_cast<int>(m_buttons.size())};
+    for (int i {m_minBtnIdx}; i < m_maxBtnIdx; i += 1) {
+        if (i >= btnsLength) {
+            break;
+        }
+
+        Button* btnPtr {m_buttons[i]};
+        btnPtr->update(renderer, mousePos, mousePressed, mouseReleased);
+        drawLockedImg(renderer, btnPtr);
+
+        if (btnPtr->clicked()) {
+            btnPtr->unclick();
+            if (btnUnlocked(btnPtr->id())) {
+                returnVal = btnPtr->id();
+            }
+        }
+    }
+
+    return returnVal;
+};
+
+
+void SelectMenu::setHighestUnlockedBtnId(int highestUnlockedId) {
+    m_highestUnlockedId = highestUnlockedId;
+
+    if (m_saveFilePath.size() != 0) {
+        saveHighestUnlockedId();
+    }
+};
+
+
+int SelectMenu::highestUnlockedBtnId() const {
+    return m_highestUnlockedId;
+};
+
+
+
+
+void SelectMenu::loadHighestUnlockedId() {
+    std::ifstream file{m_saveFilePath};
+    if (file.fail()) {
+        file.close();
+        return;
+    }
+
+    try {
+        std::string savedId{};
+        std::getline(file, savedId);
+        m_highestUnlockedId = std::stoi(savedId);
+        file.close();
+    } catch (...) {
+        file.close();
+        m_highestUnlockedId = 0;
+    }
+};
+
+
+void SelectMenu::saveHighestUnlockedId() {
+    std::ofstream file{m_saveFilePath};
+    if (file.fail()) {
+        file.close();
+        return;
+    }
+
+    try {
+        file << std::to_string(m_highestUnlockedId);
+        file.close();
+    } catch (...) {
+        file.close();
+    }
+};
+
+
+void SelectMenu::drawLockedImg(SDL_Renderer* renderer, Button* btnPtr) {
+    if (btnUnlocked(btnPtr->id())) {
+        return;
+    }
+
+    m_lockedImgRect.setCenter(btnPtr->center());
+    SDL_RenderTexture(renderer, m_btnLockedImg, nullptr, &m_lockedImgRect.getSDLFRect());
+};
+
+
+bool SelectMenu::btnUnlocked(int btnId) {
+    if (!m_btnLockedImg) {
+        return true;
+    }
+
+    return btnId <= m_highestUnlockedId;
+};
+
+
+void SelectMenu::pageButtons(bool pageUp) {
+    if (pageUp && m_minBtnIdx == 0) {
+        return;
+    }
+    if (!pageUp && m_maxBtnIdx >= static_cast<int>(m_buttons.size())) {
+        return;
+    }
+
+    int idxChange {(pageUp) ? -m_idxChange : m_idxChange};
+    m_minBtnIdx += idxChange;
+    m_maxBtnIdx += idxChange;
+};
+
+
+void SelectMenu::positionPageAndExitBtns(
+    Button* exitBtn, 
+    Button* pageUpBtn, 
+    Button* pageDownBtn, 
+    float colHeight, 
+    float btnGap, 
+    float centerX, 
+    float centerY
+) {
+    float btnsAreaTop {centerY - (colHeight / 2.0f)};
+    float btnsAreaBottom {centerY + (colHeight / 2.0f)};
+
+    pageUpBtn->setCenter(centerX, btnsAreaTop - btnGap - (pageUpBtn->height() / 2.0f));
+    pageUpBtn->setId(m_pageUpId);
+    m_interfaceBtns.push_back(pageUpBtn);
+
+    pageDownBtn->setCenter(centerX, btnsAreaBottom + btnGap + (pageDownBtn->height() / 2.0f));
+    pageDownBtn->setId(m_pageDownId);
+    m_interfaceBtns.push_back(pageDownBtn);
+
+    auto [pageDownX, pageDownY] {pageDownBtn->topleft()};
+    exitBtn->setCenter(
+        centerX, 
+        pageDownY + pageDownBtn->height() + (btnGap * 2) + (exitBtn->height() / 2.0f)
+    );
+    exitBtn->setId(m_exitId);
+    m_interfaceBtns.push_back(exitBtn);
+};
+
+
+float SelectMenu::positionBtns(int numRows, int numCols, float centerX, float centerY, float btnGap) {
+    auto [maxBtnWidth, maxBtnHeight] {maxBtnSize()};
+    float rowWidth {maxBtnWidth * numCols + (btnGap * (numCols - 1))};
+    float colHeight {maxBtnHeight * numRows + (btnGap * (numRows - 1))};
+
+    float firstRowY {centerY - (colHeight / 2.0f)};
+    float firstColX {centerX - (rowWidth / 2.0f)};
+
+
+    int btnIdx {0};
+    int btnsLength {static_cast<int>(m_buttons.size())};
+    while (btnIdx < btnsLength) {
+        float currentY {firstRowY};
+
+        for (int row {0}; row < numRows; row += 1) {
+            float currentX {firstColX};
+
+            for (int col {0}; col < numCols; col += 1) {
+                if (btnIdx >= static_cast<int>(m_buttons.size())) {
+                    break;
+                }
+
+                Button* btn {m_buttons[btnIdx]};
+
+                float btnCenterX {currentX + (maxBtnWidth / 2.0f)};
+                float btnCenterY {currentY + (maxBtnHeight / 2.0f)};
+                btn->setCenter(btnCenterX, btnCenterY);
+                btn->setId(btnIdx);
+
+                currentX += maxBtnWidth + btnGap;
+                btnIdx += 1;
+            }
+
+            currentY += maxBtnHeight + btnGap;
+        }
+    }
+
+    m_maxBtnIdx = numRows * numCols;
+    m_idxChange = m_maxBtnIdx;
+    return colHeight;
+};
+
+
+std::pair<float, float> SelectMenu::maxBtnSize() {
+    float maxWidth {0};
+    float maxHeight {0};
+
+    for (Button* btnPtr : m_buttons) {
+        float btnWidth {btnPtr->width()};
+        maxWidth = (maxWidth < btnWidth) ? btnWidth : maxWidth;
+
+        float btnHeight {btnPtr->height()};
+        maxHeight = (maxHeight < btnHeight) ? btnHeight : maxHeight;
+    }
+
+    return std::pair<float, float>{maxWidth, maxHeight};
+};
